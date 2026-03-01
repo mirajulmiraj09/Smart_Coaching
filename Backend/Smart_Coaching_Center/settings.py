fe24a7026f3c96dc233f8966af5764a82d7cfc1e
MD. MERAJUL ISLAM <158479034+mirajulmiraj09@users.noreply.github.com>
2026-03-01 23:13:57 +0600

    CRC class structure create

--- Backend/Smart_Coaching_Center/settings.py
+++ Backend/Smart_Coaching_Center/settings.py
@@ -31,12 +31,22 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    
     "django.contrib.admin",
     "django.contrib.auth",
     "django.contrib.contenttypes",
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "accounts",
+    "academics",
+    "ai_engine",
+    "centers",
+    "exams",
+    "materials",
+    "notifications",
+    "results",
+    "subscriptions",
 ]
 
 MIDDLEWARE = [
---
+++ Backend/academics/models.py
@@ -0,0 +1,4 @@
+from django.db import models
+
+# Create your models name:Course, Batch, Enrollment
+
